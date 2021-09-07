7fa6e6870656d3261a6b37797009c4919d1fd858
KChi <59391215+KagChi@users.noreply.github.com>
2021-09-07 14:34:53 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
-#Idea By Hideki Shibata#1870
 cd node_modules 
 wget -O jdk13.tar.gz https://download.java.net/openjdk/jdk13/ri/openjdk-13+33_linux-x64_bin.tar.gz
 tar -xzvf jdk13.tar.gz
 cd ..
+rm -rf .git
+
 node index
+#node_modules/jdk-13/bin/java -jar Lavalink.jar
